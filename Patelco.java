f60a8f7cf06402dc2f787c7815b7bb10b2d181ac
Dhruven Shah <Dhruven_10@yahoo.co.in>
2018-10-16 14:36:45 -0700

    Added Pateco Program

---
+++ Patelco.java
@@ -0,0 +1,11 @@
+public class Patelco{
+
+	public static void main (String[] args){
+
+		for (int i = 1; i <= 10; i++){
+
+			System.out.println("This is a sample Patelco Job..." + i);
+
+		}
+	}
+}
